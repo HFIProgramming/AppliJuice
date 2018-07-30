ce36ffb0b18b2e6787972668ee744ee5f7bd37cb
ZihongLin <1046298030@qq.com>
2018-07-30 02:01:36 -0400

    ApplicationController some outline

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -7,4 +7,27 @@ use Illuminate\Http\Request;
 class ApplicationController extends Controller
 {
     //
+    public function __construct()
+    {
+    }
+
+    public function newApplicationForm()
+    {
+
+    }
+
+    public function createApplication(Request $request)
+    {
+
+    }
+
+    public function reviewApplication()
+    {
+
+    }
+
+    public function editApplicantInfo(Request $request)
+    {
+
+    }
 }
